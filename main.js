f5116684d60a1ac1042689e2eaac9659219fb53c
Edwin Roeleveld <peepastrider@gmail.com>
2024-05-21 13:19:26 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,43 @@
+const http = require('http');
+const sqlite3 = require('sqlite3').verbose();
+const express = require('express');
+
+const app = express();
+const port = 5500;
+
+// Database connection
+const db = new sqlite3.Database('database.db', (err) =>{
+    if (err) {
+        console.error('Error connecting to the database.');
+    }
+    else{
+        console.log('Opened connection to the database.');
+    }
+});
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log(`Received request: ${req.method} ${req.url}`);
+    next();
+});
+
+app.get('/api/cars', (req, res) => {
+    const query = 'SELECT * FROM Car';
+
+    db.all(query, (err, rows) =>{
+        if (err) {
+            console.error(err.message);
+            res.status(500).json('Internal Server Error');
+            return;
+        }
+        res.json(rows);
+    });
+});
+
+// Start server
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
+
+
